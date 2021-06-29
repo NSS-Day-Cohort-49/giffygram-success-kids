96d132736a7a43ce48446e518f3cd62029a8833f
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-06-29 09:30:19 -0500

    created navabar with some css styling; added styling to login page and form; modified postlist html

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -1,9 +1,12 @@
 import { Posts } from "./feed/PostList.js"
+import { NavBar } from "./nav/NavBar.js"
 
 export const GiffyGram = () => {
 
     // Show main main UI
-    return `<h1>Giffygram</h1>
+    return `
+    ${NavBar()}
+   
     
     <section class="posts">
         <h2>Posts</h2>
--- src/scripts/feed/PostList.js
+++ src/scripts/feed/PostList.js
@@ -1,30 +1,26 @@
 import { getPosts } from "../data/provider.js"
 
-// export const Posts = () => {
-//     const posts = getPosts()
 
-//     let html = `
-//         <ul>
-//             ${
-//                 posts.map(
-//                       (post) => `<li>
-//                         <input type="hidden" value="${post.id}" name="post"> ${post.title} ${post.url} ${post.desciption}
-//                       </li>`).join("")
-//             }
-//         </ul>
-//     `
-
-//     return html
-// }
 
 export const Posts = () => {
     const posts = getPosts();
     console.log(posts)
+
     let postHTML = `${posts.map(post => {
-        return `<li>
-        <input type="hidden" value="${post.id}" name="post"> ${post.title} <img src="${post.url}"/> ${post.desciption}
-        </li>`}).join("")
-    }`
+        return `<section>
+            <h1 class="post_title">${post.title}</h1>
+            <img class="post_img" src="${post.url}"/>
+            <section class="post_description">${post.description}</section>
+            <button class="post_favorite" id="favorite--${post.id}"><img src="https://img.icons8.com/material-outlined/24/000000/christmas-star.png"/></button>
+            <button class="post_remove" id="remove--${post.id}"><img src="https://img.icons8.com/dusk/24/000000/trash.png"/></button>
+        </section>`
+    }).join("")}`
+
+    // let postHTML = `${posts.map(post => {
+    //     return `<li>
+    //     <input type="hidden" value="${post.id}" name="post"> ${post.title} <img src="${post.url}"/> ${post.description}
+    //     </li>`}).join("")
+    // }`
 
     return postHTML;
-}
+};
